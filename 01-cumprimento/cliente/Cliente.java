79bc351785e08575d99a79cfefbc6e722d8f7759
Talisson Silva <talissonvsilva@gmail.com>
2026-09-25 20:43:53 -0300

    Update Cliente.java

--- 01-cumprimento/cliente/Cliente.java
+++ 01-cumprimento/cliente/Cliente.java
@@ -1,27 +1,21 @@
 package cliente;
 
-import java.io.*;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-
-import servico.Cumprimento;
+import servico.GeradorAleatorio;
+import java.rmi.Naming;
 
 public class Cliente {
-  public static void main(String[] args) {
-    String host = (args.length < 1) ? null : args[0];
-    
-    try {
-      Registry registry = LocateRegistry.getRegistry(host);
-      Cumprimento stub = (Cumprimento) registry.lookup("ServiçoDeCumprimentoRemoto");
-
-      String resposta = stub.cumprimentar();
-      
-      System.out.println("Resposta do servidor: " + resposta);
-      //InputStreamReader in = new InputStreamReader(System.in);
-      //in.read();
-    } catch (Exception e) {
-      System.err.println("Erro no cliente: " + e.toString());
-      e.printStackTrace();
+    public static void main(String[] args) {
+        try {
+            System.out.println("A procurar o serviço no servidor...");
+            
+            GeradorAleatorio servico = (GeradorAleatorio) Naming.lookup("rmi://localhost:1099/ServicoAleatorio");
+            
+            int resultado = servico.gerarNumero();
+            
+            System.out.println("Sucesso! Número aleatório recebido do servidor: " + resultado);
+            
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro no cliente: " + e.getMessage());
+        }
     }
-  }
 }
